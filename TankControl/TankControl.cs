e4a9ae013dfe8b6e33136cb6039c6d7d54f8624f
Curtis Brandt <curtis@cpan.org>
2017-09-07 21:24:52 -0700

    Update TankControl.cs

--- TankControl/TankControl.cs
+++ TankControl/TankControl.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class TankControl : MonoBehaviour {
     public float walkSpeed;
     public float rotateSpeed;
+    private CharacterController controller;
+    private Animator animator;
 
-    // Use this for initialization
-    void Start () {
-    }
+	// Use this for initialization
+	void Start () {
+        controller = new CharacterController();
+        animator = (Animator) GetComponent<Animator>();
+        Debug.Log(animator);
+
+	}
 
     // Update is called once per frame
     void Update() {
@@ -19,13 +25,15 @@ public class TankControl : MonoBehaviour {
         // If walking backwards, reduce speed.
         if (v > 0) {
             Vector3 forward = transform.TransformDirection(Vector3.forward);
-            transform.Translate(0, 0, forward.magnitude * (walkSpeed * v));
+            transform.Translate(0, 0, forward.magnitude * (walkSpeed * v) / 2);
         } else if (v < 0) {
             Vector3 back = transform.TransformDirection(Vector3.back);
-            transform.Translate(0, 0, back.magnitude * (walkSpeed * v) / 4);
+            transform.Translate(0, 0, back.magnitude * (walkSpeed * v) / 5);
         }
 
         // Rotate model when L/R is pressed.
         transform.Rotate(0, h * rotateSpeed, 0);
+
+        animator.SetFloat("Forward", v);
     }
 }
